ec93f674de8368a5d6bb10d7ba0a3b0e0a135508
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-09 16:38:40 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+ new_hash={}
+
+languages.each do |style, language|
+    
+    language.each do |name, info|
+      
+      info.each do |type, value|
+    
+        new_hash[name] ||= {type: value}
+        
+        new_hash[name][:style] ||= [] 
+        new_hash[name][:style] << style
+        
+     end  
+  end 
+end 
+  new_hash
 end
